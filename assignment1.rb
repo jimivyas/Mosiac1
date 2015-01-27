5778d04d14117d5a44fde2fefd4a6216d06bb94e
Jimi Vyas <jimipvyas@gmail.com>
2015-01-27 13:28:21 -0500

    add completed file

--- assignment1.rb
+++ assignment1.rb
@@ -1,50 +1,64 @@
-
-# input is array of hashes
-# output is array of hashes
-
-
 class Events
-	def initialize
-		$output ||= Array.new
+	def is_array?(obj)
+		obj.is_a? Array
+	end
+
+	def all_hashes?(array)
+		array.all? do |element|
+			element.is_a? Hash
+		end
+	end
+
+	def date?(stringnum)
+		stringnum.to_i.between?(1, 31)
+	end
+
+	def month?(stringnum)
+		stringnum.to_i.between?(1, 12)
+	end
+
+	def year?(stringnum)
+		stringnum.to_i.between?(0, 3000)
 	end
 
 
-	def date_removed(event)
-		event.tap do |e| #tap returns the original event, which is why the method has a passive voice
-			e.delete(:date)
+	def date_presence?(array)
+		array.all? do |element|
+			element[:date].is_a? String and date_formatted_right?(element[:date])
 		end
 	end
 
 
-	def store_all_events(input_hash, output_hash)
-		event_date = output_hash[:date]
-		date_removed(input_hash).each do |k, v|
-			$output.find {|day| day[:date] == event_date }[k] = v
-		end
+	def date_formatted_right?(string)
+		year?(string[0,4]) and month?(string[5, 2]) and date?(string[8, 2])
 	end
 
-	def make_date_hash(event_array)
-		$output << event_array.first
-		puts $output.first[:date]
-		event_array.each do |event|
-			$output.clone.each do |day|
-				if event[:date] == day[:date]
-					store_all_events(event, day)
-				else
-					$output << event
-				end
-			end
+
+	def valid_test(events)
+		is_array?(events) and all_hashes?(events) and date_presence?(events)
+	end
+
+
+	def make_date_hash(events)
+		if valid_test(events)
+			events = events.group_by { |e|	e.first }
+			events = events.map { |_, e| #underscore is for blank variables
+				e.inject(:merge)}
+			puts events
+		else
+			puts 'Your input was not properly formatted.'
 		end
-		puts $output
 	end
 end
 
-random_dates = [
+
+randevents = [
   {date: '2014-01-01', a: 5, b:1},
   {date: '2014-01-01', xyz: 11},
   {date: '2014-10-10', qbz: 5},
-  {date: '2014-10-10', v: 4, q: 1, strpm: -99} ]
-
+  {date: '2014-10-10', v: 4, q: 1, strpm: -99},
+  {date: '2014-10-10', j: 4, fj: 5}
+]
 
 calendar = Events.new
-calendar.make_date_hash(random_dates)
+calendar.make_date_hash(randevents)

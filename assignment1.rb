a8a5a96df365f080691d89899d3be9ffd80e8c2e
Jimi Vyas <jimipvyas@gmail.com>
2015-01-27 13:31:45 -0500

    add instruction within file

--- assignment1.rb
+++ assignment1.rb
@@ -21,23 +21,19 @@ class Events
 		stringnum.to_i.between?(0, 3000)
 	end
 
-
 	def date_presence?(array)
 		array.all? do |element|
 			element[:date].is_a? String and date_formatted_right?(element[:date])
 		end
 	end
 
-
 	def date_formatted_right?(string)
 		year?(string[0,4]) and month?(string[5, 2]) and date?(string[8, 2])
 	end
 
-
 	def valid_test(events)
 		is_array?(events) and all_hashes?(events) and date_presence?(events)
 	end
-
 
 	def make_date_hash(events)
 		if valid_test(events)
@@ -51,7 +47,7 @@ class Events
 	end
 end
 
-
+#date-based events go here, feel fry to try broken ones :)
 randevents = [
   {date: '2014-01-01', a: 5, b:1},
   {date: '2014-01-01', xyz: 11},
